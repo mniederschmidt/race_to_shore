f41097390154d848e8925a50956562896aa3d7df
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2018-04-11 21:57:32 -0500

    Created racer object and made it race

--- racing.js
+++ racing.js
@@ -2,30 +2,65 @@
     Your Object Definition
 *******************************************************************************/
 
-// TODO: define your object here
+var submarine = {
+
+  distanceFromShore: 100,
+  hasEngine: true,
+  color: "yellow",
+  speed: "fast",
+  engineOn: true,
+
+  moveCloserToShore: function() {
+    console.log("distance from shore: " + this.distanceFromShore);
+    switch(this.speed) {
+      case "fast":
+        this.distanceFromShore -= 10;
+        break;
+      case "slow":
+        this.distanceFromShore -= 1;
+        break;
+      default:
+        this.distanceFromShore -= 5;
+        break;
+    };
+  },
+  turnEngineOn: function() {
+    this.engineOn = true;
+  },
+  turnEngineOff: function() {
+    this.engineOn = false;
+  },
+  resetRace: function(distance) {
+    this.distanceFromShore = distance;
+  },
+};
 
 
 /*******************************************************************************
     Running Race Code
 *******************************************************************************/
 
-var racer;
+var racer = submarine;
+racer.color = "yellow";
+racer.speed = "fast";
 
-function runRace()
-{
-    // TODO: fill code here
+function runRace() {
+
+  while (racer.distanceFromShore >= 0) {
+    racer.moveCloserToShore(racer.speed);
+  }
+  console.log("FINISHED RACE");
 }
 
-function resetRace()
-{
-    // TODO: fill code here
+function resetRace() {
+  racer.resetRace(100);
 }
 
 /*******************************************************************************
     jquery definitions
     these connect the javascript defined above to the html
 *******************************************************************************/
-$(document).on('ready', function(){
+$(document).ready(function(){
 
     // the inside function runs when the start race button is clicked
     $('#start-race').on('click', function (){
